abac57884f0b9bfabec08944f9cd1ee2e45cc4cc
fran-itria <francoitria01@gmail.com>
2026-08-24 19:07:38 -0300

    fix: update import styles and enhance error handling in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from "@nestjs/schedule";
+import { ScheduleModule } from '@nestjs/schedule';
 import { databaseImports } from './config/database';
-
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 const domainModules =
-  process.env.NODE_ENV === 'test'
-    ? []
-    : [
-    ];
+  process.env.NODE_ENV === 'test' ? [] : [
+    AuthModule,
+    UsersModule
+  ];
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     ...databaseImports,
-    ...domainModules
+    ...domainModules,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 try {
-  process.loadEnvFile()
-} catch (error) {
-}
+  process.loadEnvFile();
+} catch (error) {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // descarta props que el DTO no declara
+      forbidNonWhitelisted: true, // y si llegan, responde 400
+      transform: true, // instancia el DTO y castea tipos primitivos
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
-  console.log("Correindo en puerto:", process.env.PORT)
+  console.log('Correindo en puerto:', process.env.PORT);
 }
 bootstrap();

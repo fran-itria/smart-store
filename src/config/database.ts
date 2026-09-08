0f67fef0104368fe47ab30dccd83f9153db0cfa5
fran-itria <francoitria01@gmail.com>
2026-09-07 21:01:22 -0300

    fix: update database configuration to use DATABASE_URL and clean up commented code

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,22 +5,22 @@ export const databaseImports =
   process.env.NODE_ENV === 'test'
     ? []
     : [
-        TypeOrmModule.forRootAsync({
-          inject: [ConfigService],
-          useFactory: (configService: ConfigService) => {
-            const rawPort = configService.get<string>('DB_PORT', '5432');
-            return {
-              type: 'postgres',
-              // url: configService.get<string>('DATABASE_URL'),
-              host: configService.get<string>('DB_HOST', 'localhost'),
-              port: Number(rawPort),
-              username: configService.get<string>('DB_USERNAME', 'postgres'),
-              password: configService.get<string>('DB_PASSWORD', 'postgres'),
-              database: configService.get<string>('DB_DATABASE', 'smart_store'),
-              autoLoadEntities: true,
-              synchronize:
-                configService.get<string>('DB_SYNCHRONIZE', 'true') === 'true',
-            };
-          },
-        }),
-      ];
+      TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+          const rawPort = configService.get<string>('DB_PORT', '5432');
+          return {
+            type: 'postgres',
+            url: configService.get<string>('DATABASE_URL'),
+            // host: configService.get<string>('DB_HOST', 'localhost'),
+            // port: Number(rawPort),
+            // username: configService.get<string>('DB_USERNAME', 'postgres'),
+            // password: configService.get<string>('DB_PASSWORD', 'postgres'),
+            // database: configService.get<string>('DB_DATABASE', 'smart_store'),
+            autoLoadEntities: true,
+            synchronize:
+              configService.get<string>('DB_SYNCHRONIZE', 'true') === 'true',
+          };
+        },
+      }),
+    ];
